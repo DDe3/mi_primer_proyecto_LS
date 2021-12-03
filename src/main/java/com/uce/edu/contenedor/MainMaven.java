3e2a486664c77dbc18df8a7de318fcad842378f9
DDe3 <lasalinasb@uce.edu.ec>
2021-12-02 19:46:56 -0500

    Segundo taller Maven

---
+++ src/main/java/com/uce/edu/contenedor/MainMaven.java
@@ -0,0 +1,11 @@
+package com.uce.edu.contenedor;
+
+public class MainMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mi primer proyecto Maven");
+
+	}
+
+}
